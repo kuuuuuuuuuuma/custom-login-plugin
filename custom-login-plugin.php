6c6f491abf7a6e2f5de323a32881a8ddccc5a16e
kuma <contact@kumatech-lab.com>
2021-12-22 19:33:54 +0900

    base

---
+++ custom-login-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Custom Login Plugin
+ * Description: ログイン周りのカスタマイズ
+ * Author: KumaTech
+ * Author URI: https://kumatech-lab.com/
+ * Version: 1.0.0
+ */
+
+require_once __DIR__ . '/class-custom-login.php';
